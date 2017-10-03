3c363cb170eb7ca1db1224b66a05d9843926258d
Pedro Coronel Santiago <coronelsantiagop@gmail.com>
2017-10-03 12:03:00 -0500

    [REF] crm_editado : Se agrgo el campo perfil sospechoso e nla vista kanban y vista partner

--- crm_editado/models/cmt_crmlead_mod.py
+++ crm_editado/models/cmt_crmlead_mod.py
@@ -5,19 +5,19 @@ from odoo import api, fields, models
 class x_crm(models.Model):
     _inherit = 'crm.lead'
 
-    x_cmt_statuscliente = fields.Boolean(
-        related='partner_id.x_cmt_validarCuestionario', default=False, readonly=True)
-    x_cmt_perfilcliente = fields.Text(
-        related='partner_id.x_perfilconfirmado', default=False, readonly=True)
+   
     x_cmt_validacion = fields.Char(
         string='Validado',
         compute='_compute_ctm_validado')
-
-    @api.depends('x_cmt_statuscliente')
+    x_cmt_sospechoso = fields.Char(
+        string='Validado', compute='_compute_ctm_validado')
+    @api.depends('partner_id.x_cmt_validarPerfil', 'partner_id.x_cmt_validarSospechoso')
     @api.one
     def _compute_ctm_validado(self):
         for record in self:
-            if record.x_cmt_statuscliente:
-                record.x_cmt_validacion = 'Validado'
+            if record.partner_id.x_cmt_validarPerfil:
+                record.x_cmt_validacion = 'Perfil Validado'
             else:
-                record.x_cmt_validacion = ' NO Validado'
+                record.x_cmt_validacion = 'Perfil NO Validado'
+            if record.partner_id.x_cmt_validarSospechoso:
+                record.x_cmt_sospechoso = 'Perfil Sospechoso'
--- crm_editado/models/cmt_mod_partner.py
+++ crm_editado/models/cmt_mod_partner.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models
 class x_cmt_partner(models.Model):
     _inherit = 'res.partner'
 
-    x_cmt_validarListaPrecios = fields.Boolean(
-        string='Validar lista de precios')
-    x_cmt_validarCuestionario = fields.Boolean(
-        string='Validar Cuestionario')
+    x_cmt_validarSospechoso = fields.Boolean(
+        string='Perfil Sospechoso')
+    x_cmt_validarPerfil = fields.Boolean(
+        string='Perfil Validado')
