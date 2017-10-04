c70a8493d6f230b57accb2ace320532a0e8718ad
Pedro Coronel Santiago <coronelsantiagop@gmail.com>
2017-10-03 19:04:08 -0500

    [REF] crm_ editado : ADD funcionalidad ṕara que agruegue  lista de precios en pase a lperfil confirmado

--- crm_editado/models/cmt_mod_partner.py
+++ crm_editado/models/cmt_mod_partner.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
-
 class x_cmt_partner(models.Model):
     _inherit = 'res.partner'
 
@@ -9,3 +8,18 @@ class x_cmt_partner(models.Model):
         string='Perfil Sospechoso')
     x_cmt_validarPerfil = fields.Boolean(
         string='Perfil Validado')
+
+    @api.onchange('x_perfilconfirmado')
+    def _asignar_lista_precios(self):
+        if self.x_perfilconfirmado == 'Proyectista' or self.x_perfilconfirmado == 'Usuario Final':
+            self.property_product_pricelist = 3
+        elif self.x_perfilconfirmado == 'Distribuidor':
+            self.property_product_pricelist = 6
+        elif self.x_perfilconfirmado == 'Profesional Tecnico':
+            self.property_product_pricelist = 4
+        elif self.x_perfilconfirmado == 'Contratista':
+            self.property_product_pricelist = 5
+        elif self.x_perfilconfirmado == 'Fabricante':
+            self.property_product_pricelist = 2
+        else:
+            self.property_product_pricelist = 1
